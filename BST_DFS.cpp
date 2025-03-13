761c292c23c36c7996cee2b6a26d7f7e294df973
Andrii Kazlitin <kerydan@gmail.com>
2025-03-13 09:02:53 +0100

    Update BST_DFS.cpp

--- BST_DFS.cpp
+++ BST_DFS.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <print>
 
 class TreeNode
 {
@@ -9,6 +9,17 @@ public:
     TreeNode* right;
 };
 
+void dfs(TreeNode *n)
+{
+    if (!n)
+        return;
+    // Pre-order traversal (Root -> Left -> Right)
+    std::println("Val = {}", n->val);
+    dfs(n->left);
+    dfs(n->right);
+    // In-order traversal (Left -> Root -> Right)
+    // Post-order traversal (Left -> Right -> Root)
+}
 
 int main()
 {
@@ -25,6 +36,12 @@ int main()
     root->left->left = new TreeNode(1);
     root->left->right = new TreeNode(4);
     
+    dfs(root);
     
+    delete root->left->left;
+    delete root->left->right;
+    delete root->left;
+    delete root->right;
+    delete root;    
     return 0;
 }
